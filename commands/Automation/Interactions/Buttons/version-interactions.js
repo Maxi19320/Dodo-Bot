af83cf2f34290568d15fcf892962edd78a0f59e7
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-03-25 17:49:09 +0200

    last minute change

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -26,10 +26,9 @@ $let[devbuild;$if[$getVar[pre_release]==on;{footer:Testing is recommended:https#
     prototype: "button",
     code: `
     $interactionUpdate[{newEmbed:{title:Bug Fixes}{description:
-* Fixed the toggle setting in \`leveling\` randomly including a \`*\` after using the button
 * Fixed internal issues related to updating "user app" commands
-* Fixed grammar of a error message seen in suggest-set and poll-set
-* Fixed "Current Setup" design in leveling, welcomer and leave changing after pressing "Toggle" button
+* Fixed grammar of a error message seen in \`suggest-set\` and \`poll-set\`
+* Fixed "Current Setup" design in \`leveling\`, \`welcomer\` and \`leave\` changing after pressing "Toggle" button
 }{color:$getVar[embedcolor]}$nonEscape[$get[devbuild]]}{actionRow:{button:Home:2:homebutton_$authorID:false:🏠}{button:Changes:2:versionchanges_$authorID:false}{button:Bug Fixes:2:versionbugfixes_$authorID:true}{button:Other:2:versionother_$authorID:false}}{actionRow:{button:Changelog history:5:https#COLON#//github.com/ddodogames/Dodo-Bot/releases:false:📜}}]
 
 $let[devbuild;$if[$getVar[pre_release]==on;{footer:Testing is recommended:https#COLON#//us-east-1.tixte.net/uploads/dodogames.wants.solutions/refreshedredwarning2.png};]]
